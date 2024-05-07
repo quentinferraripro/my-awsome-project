b0258a8a1d5268a934108f41329ac5c8e15dc426
Quentin Ferrari <108803172+quentinferraripro@users.noreply.github.com>
2024-05-07 13:51:29 +0200

    Create testService.java

---
+++ src/main/java/com/example/demo/testService.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.testService;
+
+@SpringBootApplication
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
