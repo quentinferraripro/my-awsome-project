db12debb93031be77ced296608ac2b50a71a796e
Quentin Ferrari <ferrari.quentin@gmail.com>
2024-05-07 15:01:47 +0200

    testservice

--- src/main/java/com/example/demo/TestService.java
+++ src/main/java/com/example/demo/TestService.java
@@ -1,12 +1,12 @@
 package com.example.demo;
 
-import org.springframework.boot.testService;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
-public class TestApplication {
+public class TestService {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+	public void String(){
+		System.out.println("hello");
 	}
-
 }
